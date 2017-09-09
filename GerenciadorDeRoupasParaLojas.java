531a035d6f30a479d506bb52fd1332f9d8ea1043
reinaldoveiga <31748036+reinaldoveiga@users.noreply.github.com>
2017-09-08 23:50:05 -0300

    Update GerenciadorDeRoupasParaLojas.java

--- GerenciadorDeRoupasParaLojas.java
+++ GerenciadorDeRoupasParaLojas.java
@@ -1,17 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class GerenciadorDeRoupasParaLojas implements GerencidorDeRoupas {
+public class GerenciadorDeRoupasParaLojas implements GerenciadorDeRoupas {
 	
 		private List<Roupas> roupas;
 		
 	public GerenciadorDeRoupasParaLojas(){
-		
 		this.roupas = new ArrayList<Roupas>();
 	}
 
 	@Override
-	public void cadastraRoupas(Roupas r) throws RoupaInexistenteException {
+	public void cadastraRoupas(Roupas r) {
 		this.roupas.add(r);
 		
 	}
@@ -24,10 +23,15 @@ public class GerenciadorDeRoupasParaLojas implements GerencidorDeRoupas {
 	@Override
 	public List<Roupas> pesquisaRoupasDofabricante() throws FabricanteInexistenteException {
 		List<Roupas> r = new ArrayList<Roupas>();
+		boolean achou = false;
 		for(Roupas ro : this.roupas){
 			if(ro.getFabricante().equals(fabricante)){
 				r.add(ro);
+				achou = true;
 			}
+		}
+		if(!achou){
+			throw new FabricanteInexistenteException("Esse fabricante não existe!");
 		}
 		return r;
 	}
@@ -35,31 +39,47 @@ public class GerenciadorDeRoupasParaLojas implements GerencidorDeRoupas {
 	@Override
 	public List<Roupas> pesquisaRoupasPorTamanho() throws TamanhoInexistenteException {
 		List<Roupas> r = new ArrayList<Roupas>();
+		boolean achou = false;
 		for(Roupas ro : this.roupas){
 			if(ro.getTamanho().equals(tamanho)){
 				r.add(ro);
+				achou = true;
 			}
+		}
+		if(!achou){
+			throw new TamanhoInexistenteException("Esse tamanho não existe!");
 		}
 		return r;
 	}
 
 	@Override
 	public List<Roupas> pesquisaRoupasPorSexo() throws SexoInexistenteException {
-List<Roupas> r = new ArrayList<Roupas>();
+		List<Roupas> r = new ArrayList<Roupas>();
+		boolean achou = false;
 		for(Roupas ro : this.roupas){
 			if(ro.getSexo().equals(sexo)){
 				r.add(ro);
+				achou = true;
 			}
 		}
-		return r;	}
+		if(!achou){
+			throw new SexoInexistenteException("Esse sexo não existe!");
+		}
+		return r;	
+	}
 
 	@Override
 	public List<Roupas> pesquisaRoupasPorTipo() throws TipoInexistenteException {
 		List<Roupas> r = new ArrayList<Roupas>();
+		boolean achou = false;
 		for(Roupas ro : this.roupas){
 			if(ro.getTipo().equals(tipo)){
 				r.add(ro);
+				achou = true;
 			}
+		}
+		if(!achou){
+			throw new TipoInexistenteException("Esse tipo de roupa não existe!");
 		}
 		return r;
 	}
@@ -73,10 +93,15 @@ List<Roupas> r = new ArrayList<Roupas>();
 	@Override
 	public void removeRoupas(String tipo, String fabricante, String tamanho, String sexo)
 			throws RoupaInexistenteException {
+		boolean achou = false;
 		for(Roupas r : this.roupas){
 			if(r.getFabricante().equals(fabricante) && r.getTamanho().equals(tamanho) && r.getSexo().equals(sexo) && r.getTipo().equals(tipo)){
 				this.roupas.remove(r);
+				achou = true;
 			}
+		}
+		if(!achou){
+			throw new RoupaInexistenteException("Essa roupa não existe!");
 		}
 		
 	}
