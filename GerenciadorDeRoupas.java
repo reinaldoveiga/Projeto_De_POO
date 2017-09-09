edec394d7138308ebe9eb1a316ec3b201792a1fa
Reinaldo Veiga <reinaldo.veiga@dce.ufpb.br>
2017-09-08 23:00:00 -0300

    adicionando o arquivo GerenciadorDeRoupas.java

---
+++ GerenciadorDeRoupas.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public interface GerenciadorDeRoupas {
+public void cadastraRoupas(Roupas r);
+	
+	public 	List<Roupas> pesquisaRoupas() throws RoupaInexistenteException;
+	
+	public 	List<Roupas> pesquisaRoupasDofabricante() throws FabricanteInexistenteException;
+	
+	public 	List<Roupas> pesquisaRoupasPorTamanho() throws TamanhoInexistenteException;
+	
+	public 	List<Roupas> pesquisaRoupasPorSexo() throws SexoInexistenteException;
+	
+	public 	List<Roupas> pesquisaRoupasPorTipo() throws TipoInexistenteException;
+	
+	public int informaQuantidadeDeRoupasCadastradas();
+	
+	public void removeRoupas(String tipo, String fabricante, String tamanho, String sexo) throws RoupaInexistenteException;;
+
+}
+
+
