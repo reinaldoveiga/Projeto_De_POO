852d82d315c8abf2c744aebee25aef62ea0f2e72
reinaldoveiga <31748036+reinaldoveiga@users.noreply.github.com>
2017-09-19 23:52:02 -0300

    Update GerenciadorDeRoupas.java

--- GerenciadorDeRoupas.java
+++ GerenciadorDeRoupas.java
@@ -2,22 +2,22 @@ import java.util.List;
 
 public interface GerenciadorDeRoupas {
 
-	public void cadastraRoupas(Roupas r);
-	
-	public 	List<Roupas> pesquisaRoupas();
-	
-	public 	List<Roupas> pesquisaRoupasDofabricante(String fabricante) throws FabricanteInexistenteException;
-	
-	public 	List<Roupas> pesquisaRoupasPorTamanho(String tamanho) throws TamanhoInexistenteException;
-	
-	public 	List<Roupas> pesquisaRoupasPorSexo(String sexo) throws SexoInexistenteException;
-	
-	public 	List<Roupas> pesquisaRoupasPorTipo(String tipo) throws TipoInexistenteException;
-	
-	public int informaQuantidadeDeRoupasCadastradas();
-	
-	public void removeRoupas(String tipo, String fabricante, String tamanho, String sexo) throws RoupaInexistenteException;;
-
+    public void cadastraRoupas(Roupas r) throws RoupaJaExisteException;
+    
+    public List<Roupas> pesquisaRoupas();
+    
+    public Roupas pesquisaRoupasPorCodigo(String codigo) throws CodigoDeBarrasInexistenteException;
+    
+    public List<Roupas> pesquisaRoupasDofabricante(String fabricante) throws FabricanteInexistenteException;
+    
+    public List<Roupas> pesquisaRoupasPorTamanho(String tamanho) throws TamanhoInexistenteException;
+    
+    public List<Roupas> pesquisaRoupasPorSexo(Sexo sexo) throws SexoInexistenteException;
+    
+    public List<Roupas> pesquisaRoupasPorTipo(String tipo) throws TipoInexistenteException;
+    
+    public int informaQuantidadeDeRoupasCadastradas();
+    
+    public void removeRoupas(String codigo) throws RoupaInexistenteException;
+    
 }
-
-
