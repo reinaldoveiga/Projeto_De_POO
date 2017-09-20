28038c3e98e50f3e81784a9338567184a40b4dbf
Reinaldo Veiga <reinaldo.veiga@dce.ufpb.br>
2017-09-20 00:34:50 -0300

    Adicionando o arquivo RoupaJaExisteException.java

---
+++ RoupaJaExisteException.java
@@ -0,0 +1,12 @@
+public class RoupaJaExisteException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    
+    public RoupaJaExisteException (String msg) {
+   	 super(msg);
+    }
+
+}
