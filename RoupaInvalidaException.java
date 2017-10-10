5300dc20a941925c184fe98c7cc66d01bfbe64e9
Reinaldo Veiga <reinaldo.veiga@dce.ufpb.br>
2017-10-10 01:41:48 -0300

    Adicionando arquivo RoupaInvalidaException.java

---
+++ RoupaInvalidaException.java
@@ -0,0 +1,13 @@
+
+public class RoupaInvalidaException extends Exception {
+	
+	/**
+    *
+    */
+	private static final long serialVersionUID = 1L;
+    
+    public RoupaInvalidaException(String msg) {
+    	super(msg);
+    }
+
+}
