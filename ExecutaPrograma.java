2008e9110f4dc92b553168b48f2b3cb3aa318db4
reinaldoveiga <31748036+reinaldoveiga@users.noreply.github.com>
2017-10-14 00:00:28 -0300

    Update and rename Executar.java to ExecutaPrograma.java

--- ExecutaPrograma.java
+++ ExecutaPrograma.java
@@ -1,18 +1,18 @@
 import javax.swing.JFrame;
 
-public class Executar {
+public class ExecutaPrograma {
 
 	public static void main(String[] args) {
 		GerenciadorDeRoupasParaLojas gerenciador = new GerenciadorDeRoupasParaLojas();
-		janela01 j = new janela01();
+		JanelaPrincipal j = new JanelaPrincipal();
 		
 		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		j.setSize(530, 260);
-        j.setSize(630, 460);
+        	j.setSize(630, 460);
 		j.setVisible(true);
 		j.setLocationRelativeTo(null);
-        j.setVisible(true);
-        j.setResizable(false);
+        	j.setVisible(true);
+        	j.setResizable(false);
         
 	}
 
