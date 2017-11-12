6a398e207d9cb55bbcc7ad3afe1a4bc52d878aff
reinaldoveiga <31748036+reinaldoveiga@users.noreply.github.com>
2017-11-12 01:41:51 -0200

    Update Roupas.java

--- Roupas.java
+++ Roupas.java
@@ -1,84 +1,96 @@
+
 public class Roupas {
     
     private String tipo, fabricante, tamanho ,codDeBarras;
     private Sexo sexo;
+    private double preco; 
     
     public Roupas(){
     	this.tipo = "";
-      	this.fabricante = "";
-      	this.sexo = null;
-      	this.tamanho = "";
-      	this.codDeBarras = "";
+    	this.tamanho = "";
+    	this.fabricante = "";
+    	this.codDeBarras = "";
+    	this.sexo = null;
+    	this.preco = 0.00;
     }
-    public Roupas(String tipo, String fabricante, Sexo sexo, String tamanho, String codigo){
-   	 this.tipo = tipo;
-   	 this.fabricante = fabricante;
-   	 this.sexo = sexo;
-   	 this.tamanho = tamanho;
-   	 this.codDeBarras = codigo;
+    public Roupas(String tipo, String fabricante, Sexo sexo, String tamanho, String codigo, double preco){
+        this.tipo = tipo;
+        this.fabricante = fabricante;
+        this.sexo = sexo;
+        this.tamanho = tamanho;
+        this.codDeBarras = codigo;
+        this.preco = preco;
     }
 
     public String getTipo() {
-   	 return tipo;
+        return tipo;
     }
 
     public void setTipo(String tipo) {
-   	 this.tipo = tipo;
+        this.tipo = tipo;
     }
 
     public String getFabricante() {
-   	 return fabricante;
+        return fabricante;
     }
 
     public void setFabricante(String fabricante) {
-   	 this.fabricante = fabricante;
+        this.fabricante = fabricante;
     }
 
     public Sexo getSexo() {
-   	 return sexo;
+        return sexo;
     }
 
     public void setSexo(Sexo sexo) {
-   	 this.sexo = sexo;
+        this.sexo = sexo;
     }
 
     public String getTamanho() {
-   	 return tamanho;
+        return tamanho;
     }
 
     public void setTamanho(String tamanho) {
-   	 this.tamanho = tamanho;
+        this.tamanho = tamanho;
     }
     
     public String getCodDeBarras() {
-   	 return codDeBarras;
+        return codDeBarras;
     }
 
     public void setCodDeBarras(String codDeBarras) {
-   	 this.codDeBarras = codDeBarras;
+        this.codDeBarras = codDeBarras;
+    }
+    
+    public double getPreco(){
+    	return this.preco;
+    }
+    public void setPreco(double preco){
+    	this.preco = preco;
     }
     
     public String toString(){
-   	 return "Tipo: "+this.tipo+", Tamanho: "+this.tamanho+", Sexo: "+this.sexo+", Fabricante: "+
-    this.fabricante+", Código de barras: "+ this.codDeBarras;
+        return "Tipo: "+this.tipo+", Tamanho: "+this.tamanho+", Sexo: "+this.sexo+", Fabricante: "+
+    this.fabricante+", Código de barras: "+ this.codDeBarras + ", Preço: " + this.preco;
     
     }
     
     public boolean equals(Object o) {
-   	 if (!(o instanceof Roupas)) {
-   		 return false;
-   	 }
-   	 
-   	 Roupas r = (Roupas) o;
-   	 if (!(r == null)) {
-   		 if (r.getCodDeBarras() == this.codDeBarras) {
-   			 return true;
-   		 } else if (!(r.getCodDeBarras() == this.codDeBarras)) {
-   			 return false;
-   		 }
-   	 }
-   	 return false;
+        if (!(o instanceof Roupas)) {
+            return false;
+        }
+        
+        Roupas r = (Roupas) o;
+        if (!(r == null)) {
+            if (r.getCodDeBarras() == this.codDeBarras) {
+                return true;
+            } else if (!(r.getCodDeBarras() == this.codDeBarras)) {
+                return false;
+            }
+        }
+        return false;
     }
     
 
 }
+
